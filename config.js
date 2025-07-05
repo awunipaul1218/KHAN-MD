2b7e990607305e2cc1894d6d4f4bb06e80c9b520
awunipaul1218 <161463801+awunipaul1218@users.noreply.github.com>
2025-07-05 09:17:56 +0000

    Update config.js
    IK~ofMTzTYD#anQw_RRjua0GzV_f4Fc0ZTh5fJkx7rCe9dv9gzL1ECQ

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
-module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-// add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-// make true or false status auto seen
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-// make true if you want auto reply on status 
+
+
+
+
+IK~ofMTzTYD#anQw_RRjua0GzV_f4Fc0ZTh5fJkx7rCe9dv9gzL1ECQ
+
+
+
+
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY KHAN-MD 🤍*",
